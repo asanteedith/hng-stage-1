34050fdc1866b09af5dcbd9c0dc7e3d8f6bbb689
Ubuntu <ubuntu@ip-172-31-27-122.eu-north-1.compute.internal>
2026-04-18 02:25:46 +0000

    Update GitHub URL in JSON response

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def read_me():
     return {
         "email": "asanteedith699@gmail.com",
         "current_datetime": "2026-04-18T00:41:28Z", 
-        "github_url": "https://github.com/asanteedith"
+        "github_url": "https://github.com/asanteedith/hng-stage-1"
     }
