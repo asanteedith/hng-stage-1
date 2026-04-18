73d03bb4e75d359aebb72d195f0dd62576257c92
Ubuntu <ubuntu@ip-172-31-27-122.eu-north-1.compute.internal>
2026-04-18 01:35:48 +0000

    Initial commit for HNG Stage 1

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/me")
+def read_me():
+    return {
+        "email": "asanteedith699@gmail.com",
+        "current_datetime": "2026-04-18T00:41:28Z", 
+        "github_url": "https://github.com/asanteedith"
+    }
